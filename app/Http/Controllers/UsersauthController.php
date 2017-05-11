eafe9a2bb75e237d42f4122463a92d83fa48f70b
Bastien <bastien.nicoud@gmail.com>
2017-05-11 23:15:54 +0200

    dump de la BD

--- app/Http/Controllers/UsersauthController.php
+++ app/Http/Controllers/UsersauthController.php
@@ -68,45 +68,8 @@ class UsersauthController extends Controller
    */
   public function register(Request $request) {
 
-    // validation des données postées
-    $validator = Validator::make($request->all(), [
-      // le champ a valider, puis les regles de validation
-      'name' => 'required|unique:users|min:6|max:100',
-      'email' => 'required|email|unique:users|max:255',
-      'password' => 'required|confirmed|min:6|max:50',
-      'password_confirmation' => 'required'
-    ], [
-      // messages d'erreurs pour chaque type de validation
-      'name.required' => 'Le nom d\'utilisateur n\'a pas été renseigné.',
-      'name.unique' => 'Ce nom d\'utilisateur existe déja.',
-      'name.min' => 'Le nom d\'utilisateur doit faire au moins 6 caractéres.',
-      'name.max' => 'Le nom d\'utilisateur peut faire maximum 100 caractéres.',
-      'email.required' => 'L\'email n\'a pas été renseigné.',
-      'email.email' => 'L\'email n\'est pas valide.',
-      'email.unique' => 'Cet email est déja utilisé pour un autre compte.',
-      'email.max' => 'L\'email peut faire maximum 200 caractéres.',
-      'password.required' => 'Le mot de passe n\'a pas été renseigné.',
-      'password.confirmed' => 'Le mot de passe n\'est pas correctement confirmé',
-      'password.min' => 'Le mot de passe doit faire au moins 6 caractéres.',
-      'password.max' => 'Le mot de passe peut faire maximum 50 caractéres.',
-      'password_confirmation.required' => 'La confirmation du mot de passe n\'a pas été renseigné.'
-    ]);
+    // appel de la fonction de connexion de laravel
 
-    // verification de l'etat de la validation
-    if($validator->fails() === true){
-      // si la validation a échoué ou renvoie les erreurs au client
-      return response()->json($validator->messages());
-    } else {
-      // si la validation est réussie on peut enregistrer l'utilisateur
-      User::create([
-        'name' => $request->input('name'),
-        'email' => $request->input('email'),
-        'password' => Hash::make($request->input('password')),
-        'image' => '/ressources/profilephotos/default.png',
-        'role' => 3
-      ]);
-      return response()->json(true);
-    }
 
   }
 
---
+++ database/dumps/tpimedia_2017-05-11.sql
@@ -0,0 +1,148 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Hôte: 127.0.0.1 (MySQL 5.7.18)
+# Base de données: tpimedia
+# Temps de génération: 2017-05-11 21:15:41 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Affichage de la table comments
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `comments`;
+
+CREATE TABLE `comments` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `content` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `user_id` int(10) unsigned NOT NULL,
+  `post_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `comments_user_id_index` (`user_id`),
+  KEY `comments_post_id_index` (`post_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+
+# Affichage de la table migrations
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `migrations`;
+
+CREATE TABLE `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+LOCK TABLES `migrations` WRITE;
+/*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`)
+VALUES
+	(1,'2014_10_12_000000_create_users_table',1),
+	(2,'2014_10_12_100000_create_password_resets_table',1),
+	(3,'2017_05_11_164028_update_users_table',1),
+	(4,'2017_05_11_164437_create_posts_table',1),
+	(5,'2017_05_11_164537_create_comments_table',1);
+
+/*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Affichage de la table password_resets
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `password_resets`;
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  KEY `password_resets_email_index` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+
+# Affichage de la table posts
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `posts`;
+
+CREATE TABLE `posts` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `slug` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date` datetime NOT NULL,
+  `hat` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `content` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `online` int(10) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `user_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `posts_title_unique` (`title`),
+  UNIQUE KEY `posts_slug_unique` (`slug`),
+  KEY `posts_user_id_index` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+
+# Affichage de la table users
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `image` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `role` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_name_unique` (`name`),
+  UNIQUE KEY `users_email_unique` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`, `image`, `role`)
+VALUES
+	(1,'jeanedouard','jd@gmail.com','$2y$10$Elt3mrsCqVplnCptVSvqnOczlUPTFbCEhJAT0sTijamdUXgbm234u',NULL,'2017-05-11 20:46:39','2017-05-11 20:46:39','/ressources/profilephotos/default.png',3),
+	(2,'YvanDuReve','ydr@gmail.com','$2y$10$E5VHPkBOw1d/L6R6Plqgr.F8v44iilWCobwG/F2vaEphMhDmVv48m',NULL,'2017-05-11 20:51:08','2017-05-11 20:51:08','/ressources/profilephotos/default.png',3),
+	(3,'legoman','man@lego.com','$2y$10$T48uv2PHlap0dVCItLj3luAb7lxbPvJwwltGh0rDPMbRRAeXHja.m',NULL,'2017-05-11 20:53:34','2017-05-11 20:53:34','/ressources/profilephotos/default.png',3),
+	(4,'SpaceViolonist','space@violon.universe','$2y$10$9dVDsuvLXeZq2Da71aTD7O6TXX1M5Emt5VLUfOgX7j1.XeOs2ff1G',NULL,'2017-05-11 21:00:41','2017-05-11 21:00:41','/ressources/profilephotos/default.png',3);
+
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
